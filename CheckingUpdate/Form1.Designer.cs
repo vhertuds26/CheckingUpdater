9111431d4f8036a05cd305d4c1bfeefc55955cf6
vhertuds26 <126396044+vhertuds26@users.noreply.github.com>
2025-07-12 20:25:39 +0800

    Update Form1.Designer.cs

--- CheckingUpdate/Form1.Designer.cs
+++ CheckingUpdate/Form1.Designer.cs
@@ -50,10 +50,10 @@
             // 
             this.button1.Location = new System.Drawing.Point(158, 193);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "Check Update";
+            this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(225, 85);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Check Update";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
