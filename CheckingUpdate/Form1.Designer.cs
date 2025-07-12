fbfafa6d9757804201a51de58ff35a0586f06068
vhertuds <vhersarmiento77@gmail.com>
2025-07-12 12:32:11 +0800

    Add project files.

---
+++ CheckingUpdate/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CheckingUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(207, 141);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(68, 16);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(207, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "version 1.0.1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 105);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CheckingUpdate/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckingUpdate
+{
+    public partial class Form1 : Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+        private async Task checkUpdate ()
+        {
+            string currentVersion = label1.Text;//version no.
+            string versionUrl = "https://example.com/version.txt";
+            string downloadUrl = "https://example.com/download";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string latestVersion = await client.GetStringAsync(versionUrl);
+                    latestVersion = latestVersion.Trim();
+
+                    if (Version.TryParse(latestVersion, out Version latest) &&
+                        Version.TryParse(currentVersion, out Version current))
+                    {
+                        if( latest > current)
+                        {
+                            DialogResult result = MessageBox.Show($"A new version ({latestVersion})is available. Do you want to Update?",
+                                "Update Available",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Information);
+                            if (result == DialogResult.Yes)
+                            {
+                                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+                                {
+                                    FileName = downloadUrl,
+                                    UseShellExecute = true
+                                });
+                                Application.Exit();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to parse version information.",
+                            "Error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Could not check for updates:\n" + ex.Message); 
+            }
+        }
+        private async void button1_Click ( object sender, EventArgs e )
+        {
+            await checkUpdate(); 
+        }
+
+    }
+}
